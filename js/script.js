8c137268e99f13331b691d5bd89c6ede6f3e34a7
ShuhratJumabaev <shuhratjumabaev11@gmail.com>
2022-09-30 18:17:20 +0500

    6

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(".banner").slick({
    arrows: false,
    slidesToShow: 1,
    slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 3000,
    responsive: [
       {
          breakpoint: 4000,
@@ -18,3 +20,38 @@ $(".banner").slick({
       },
    ],
 });
+$(".sliders").slick({
+   dots: true,
+   arrows: true,
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   variableWidth: true,
+   autoplay: true,
+   autoplaySpeed: 3000,
+   nextArrow:
+      '<button class="slick-next"><img src="./img/right-arrow.png" alt=""></button>',
+   prevArrow:
+      '<button class="slick-prev"><img src="./img/left-arrow.png" alt=""></button>',
+   responsive: [
+      {
+         breakpoint: 750,
+         settings: {
+            arrows: false,
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+         },
+      },
+      {
+         breakpoint: 1170,
+         settings: {
+            dots: true,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+         },
+      },
+   ],
+});
+
